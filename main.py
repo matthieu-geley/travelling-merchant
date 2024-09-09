da3e9edd792c055137a5b3460a4a9d84a1e74660
Matthieu <115145423+matthieu-geley@users.noreply.github.com>
2024-09-09 16:53:06 +0200

    2D plot + map plot

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import mpl_toolkits
+from mpl_toolkits.basemap import Basemap
 
 df = pd.read_csv('./Docs/villes_france_lat_long.csv', sep=',')
 
 print(df.head())
+
+# affichage 2D des points
+
+plt.scatter(df['Latitude'], df['Longitude'])
+# plt.show()
+
+# utilisation d'un globe terrestre pour afficher les points sur une carte en 3D zoomée sur la France
+
+m = Basemap(projection='ortho', lat_0=45, lon_0=0)
+
+m.drawcoastlines()
+m.drawcountries()
+m.drawmapboundary(fill_color='aqua')
+m.fillcontinents(color='coral', lake_color='aqua')
+
+x, y = m(df['Longitude'].values, df['Latitude'].values)
+
+m.scatter(x, y, color='red')
+
+plt.show()
